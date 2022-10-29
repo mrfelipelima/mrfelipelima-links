be5729ebc73da196f9f5f91566739ccffb3960c4
Felipe Lima <mrfelipelima@gmail.com>
2022-10-29 19:14:49 -0300

    fix(_app): fixed gtmId to get tag from environment variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    TagManager.initialize({ gtmId: process.env.GTM_ID })
+    TagManager.initialize({ gtmId: `${process.env.GTM_ID}` })
   }, [])
   return <Component {...pageProps} />
 }
