2a1b6abe3888150a7326b8c5ea3ba6b97d3eaba6
Felipe Lima <mrfelipelima@gmail.com>
2022-10-29 19:10:36 -0300

    feat(_app): added Google Tag Manager code with react-gtm-module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<Component {...pageProps} />)
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.GTM_ID })
+  }, [])
+  return <Component {...pageProps} />
 }
