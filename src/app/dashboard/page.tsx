c96978bde7247eb8c8e95b4e3dc991b9efaaa304
Felipe Lima <mrfelipelima@gmail.com>
2023-09-05 01:24:51 -0300

    feat(LinkList): now link list gets data from notion database

--- src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Dashboard() {
-  return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h1>Dashboard</h1>
-    </main>
-  )
-}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-const apiURL = process.env.NEXT_PUBLIC_API_URL
+const notionToken = process.env.NOTION_ACCESS_TOKEN;
 
-const localApi = axios.create({
-  baseURL: apiURL || '/api/v1'
+const notionApi = axios.create({
+  baseURL: process.env.NOTION_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${notionToken}`,
+    'Notion-Version': '2022-06-28',
+    'Content-Type': 'application/json'
+  }
 })
 
-export { localApi };
+export { notionApi };
