4d7b501e3493106901e70a99970d4c0ebdef5714
Felipe Lima <mrfelipelima@gmail.com>
2024-11-19 15:36:52 -0300

    fix: switch urlHandle to use NEXT_PUBLIC_VERCEL_URL

--- src/lib/url-handler.ts
+++ src/lib/url-handler.ts
@@ -4,7 +4,7 @@ export function urlHandler() {
   const protocol = env.NODE_ENV === 'production' ? 'https' : 'http'
   const host =
     env.NODE_ENV === 'production'
-      ? process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
       : 'localhost:3000'
   const url = `${protocol}://${host}`
   return url
